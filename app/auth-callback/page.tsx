45aa2095be8e4a89701b23256a8c176b4f20e8f4
Itteba Gilani <itteba1@gmail.com>
2024-08-14 11:49:10 -0400

    update

--- app/auth-callback/page.tsx
+++ app/auth-callback/page.tsx
@@ -1,17 +1,16 @@
 "use client";
 
-import { Suspense, useEffect } from "react"; // Import useEffect
+import { useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { trpc } from "../_trpc/client";
 import { Loader2 } from "lucide-react";
+import { Suspense } from "react"; // Import Suspense
 
 const Page = () => {
   const router = useRouter();
-
   const searchParams = useSearchParams();
   const origin = searchParams.get("origin");
 
-  // Store the query result in a variable
   const { data, error, isSuccess, isError } = trpc.authCallback.useQuery(
     undefined,
     {
@@ -20,14 +19,12 @@ const Page = () => {
     }
   );
 
-  // Handle success in useEffect
   useEffect(() => {
     if (isSuccess && data?.success) {
       router.push(origin ? `/${origin}` : "/dashboard");
     }
   }, [isSuccess, data, router, origin]);
 
-  // Handle error in useEffect
   useEffect(() => {
     if (isError && error.data?.code === "UNAUTHORIZED") {
       router.push("/sign-in");
@@ -35,7 +32,15 @@ const Page = () => {
   }, [isError, error, router]);
 
   return (
-    <Suspense>
+    <Suspense fallback={
+      <div className="w-full mt-24 flex justify-center">
+        <div className="flex flex-col items-center gap-2">
+          <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+          <h3 className="font-semibold text-xl">Setting up your account...</h3>
+          <p>You will be redirected automatically.</p>
+        </div>
+      </div>
+    }>
       <div className="w-full mt-24 flex justify-center">
         <div className="flex flex-col items-center gap-2">
           <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
