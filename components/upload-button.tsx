b18fe3d0b64a371b9cda5a7eab7ddb0b9a7b7870
Itteba Gilani <itteba1@gmail.com>
2024-08-14 22:49:49 -0400

    update

--- components/upload-button.tsx
+++ components/upload-button.tsx
@@ -27,6 +27,7 @@ const UploadDropzone = () => {
     retry: true,
     retryDelay: 500,
   });
+
   const startSimulatedProgress = () => {
     setUploadProgress(0);
 
@@ -46,11 +47,11 @@ const UploadDropzone = () => {
   return (
     <Dropzone
       multiple={false}
-      onDrop={async (acceptedFile) => {
+      onDrop={async (acceptedFiles) => {
         setIsUploading(true);
 
         const progressInterval = startSimulatedProgress();
-        const res = await startUpload(acceptedFile);
+        const res = await startUpload(acceptedFiles);
 
         if (!res) {
           return toast({
@@ -82,55 +83,43 @@ const UploadDropzone = () => {
       {({ getRootProps, getInputProps, acceptedFiles }) => (
         <div
           {...getRootProps()}
-          className="border h-64 m-4 border-dashed border-gray-300 rounded-lg"
+          className="border h-64 m-4 border-dashed border-gray-300 rounded-lg cursor-pointer"
         >
-          <div className="flex items-center justify-center h-full w-full">
-            <label
-              htmlFor="dropzone-file"
-              className="flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
-            >
-              <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                <Cloud className="h-6 w-6 text-zinc-500 mb-2" />
-                <p className="mb-2 text-sm text-zinc-700">
-                  <span className="font-semibold">Click to upload</span> or drag
-                  and drop.
-                </p>
-                <p className="text-xs text-zinc-500">PDF (up to 4 MB)</p>
+          <input
+            {...getInputProps()}
+            className="hidden"
+          />
+          <div className="flex flex-col items-center justify-center h-full w-full">
+            <Cloud className="h-6 w-6 text-zinc-500 mb-2" />
+            <p className="mb-2 text-sm text-zinc-700">
+              <span className="font-semibold">Click to upload</span> or drag
+              and drop.
+            </p>
+            <p className="text-xs text-zinc-500">PDF (up to 4 MB)</p>
+            {acceptedFiles && acceptedFiles[0] && (
+              <div className="max-w-xs bg-white flex items-center rounded-md overflow-hidden outline outline-[1px] outline-zinc-200 divide-x divide-zinc-200">
+                <div className="px-3 py-2 h-full grid place-items-center">
+                  <FileIcon className="w-4 h-4 text-blue-500" />
+                </div>
+                <div className="px-3 py-2 h-full text-sm truncate">
+                  {acceptedFiles[0].name}
+                </div>
               </div>
-
-              {acceptedFiles && acceptedFiles[0] ? (
-                <div className="max-w-xs bg-white flex items-center rounded-md overflow-hidden outline outline-[1px] outline-zinc-200 divide-x divide-zinc-200">
-                  <div className="px-3 py-2 h-full grid place-items-center">
-                    <FileIcon className="w-4 h-4 text-blue-500" />
+            )}
+            {isUploading && (
+              <div className="w-full mt-4 max-w-xs mx-auto">
+                <Progress
+                  value={uploadProgress}
+                  className="h-1 w-full bg-zinc-200"
+                  indicatorColor={uploadProgress === 100 ? "bg-green-500" : ""}
+                />
+                {uploadProgress === 100 && (
+                  <div className="flex gap-1 items-center justify-center text-sm text-zinc-700 text-center pt-2">
+                    <Loader2 className="h-3 w-3 animate-spin" /> Redirecting...
                   </div>
-                  <div className="px-3 py-2 h-full text-sm truncate">
-                    {acceptedFiles[0].name}
-                  </div>
-                </div>
-              ) : null}
-
-              {isUploading ? (
-                <div className="w-full mt-4 max-w-xs mx-auto">
-                  <Progress
-                    value={uploadProgress}
-                    className="h-1 w-full bg-zinc-200"
-                    indicatorColor={uploadProgress === 100 ? "bg-green-500" : ""}
-                  />
-                  {uploadProgress === 100 ? (
-                    <div className="flex gap-1 items-center justify-center text-sm text-zinc-700 text-center pt-2">
-                      <Loader2 className="h-3 w-3 animate-spin"/> Redirecting...
-                    </div>
-                  ): null}
-                </div>
-              ) : null}
-
-              <input
-                type="file"
-                id="dropzone-file"
-                className="hidden"
-                {...getInputProps}
-              />
-            </label>
+                )}
+              </div>
+            )}
           </div>
         </div>
       )}
